6986889ce2deaac21d4472b801f7fc4b2be07e26
Florian Baumann <flo@noqqe.de>
2022-09-27 10:31:32 +0200

    reformat

--- init.lua
+++ init.lua
@@ -4,42 +4,43 @@ local o = vim.opt
 local fn = vim.fn
 
 -- General
-o.history = 1000                        -- lots of command line history
-o.encoding = "utf8"                     -- be nice, by default
-o.cf = true                             -- error files / jumping
-o.ffs = "unix,dos,mac"                  -- support these files
-o.modeline = true                       -- make sure modeline support is enabled
-o.autoread = true                       -- reload files (no local changes only)
-o.tabpagemax = 50                       -- open 50 tabs max
-o.shortmess = "filtIoOA"                 -- shorten messages
-o.visualbell = false                    -- Disable visual bell
-o.showmode = false                      -- Disable Showmode since its in airline
+o.history = 1000                    -- lots of command line history
+o.encoding = "utf8"                 -- be nice, by default
+o.cf = true                         -- error files / jumping
+o.ffs = "unix,dos,mac"              -- support these files
+o.modeline = true                   -- make sure modeline support is enabled
+o.autoread = true                   -- reload files (no local changes only)
+o.tabpagemax = 50                   -- open 50 tabs max
+o.shortmess = "filtIoOA"            -- shorten messages
+o.visualbell = false                -- Disable visual bell
+o.showmode = false                  -- Disable Showmode since its in airline
 
 -- UI Changes
-o.ruler = true                   -- show the cursor position all the time
-o.showcmd = false                -- don't display incomplete commands
-o.lazyredraw = false             -- turn off lazy redraw
-o.wildmenu = true                -- turn on wild menu
-o.ch = 1                         -- command line height
-o.report = 0                     -- tell us about changes
-o.startofline = false            -- don't jump to the start of line when scrolling
-o.wildmode = "list:longest,full" -- nice completion for wildcards
-o.number = true                  -- enable line numbers by default
-o.hlsearch = true                -- highlight searches
-o.mouse = "r"                    -- turn off mouse
-o.clipboard:append("unnamed")    -- persistent cut buffer across files (dd + p works everywhere)
-o.incsearch = false              -- don't jump during searching for a string
-o.autochdir = true               -- automatically switch to dir of file editing
+o.ruler = true                      -- show the cursor position all the time
+o.showcmd = false                   -- don't display incomplete commands
+o.lazyredraw = false                -- turn off lazy redraw
+o.wildmenu = true                   -- turn on wild menu
+o.ch = 1                            -- command line height
+o.report = 0                        -- tell us about changes
+o.startofline = false               -- don't jump to the start of line when scrolling
+o.wildmode = "list:longest,full"    -- nice completion for wildcards
+o.number = true                     -- enable line numbers by default
+o.hlsearch = true                   -- highlight searches
+o.mouse = "r"                       -- turn off mouse
+o.clipboard:append("unnamed")       -- persistent cut buffer across files (dd + p works everywhere)
+o.incsearch = false                 -- don't jump during searching for a string
+o.autochdir = true                  -- automatically switch to dir of file editing
+o.whichwrap = o.whichwrap .. "<,>"  -- Wrap movement between lines in edit mode with arrows
 
 -- Text Formatting
-o.autoindent = true         -- automatic indent new lines
-o.smartindent = true        -- be smart about it
-o.wrap = false              -- do not wrap lines
-o.expandtab = true          -- expand tabs to spaces
-o.smarttab = false          -- fuck tabs
-o.formatoptions:append("n") -- support for numbered/bullet lists
-o.virtualedit = "block"     -- allow virtual edit in visual block ..
-o.scrolloff = 4             -- scroll down and let 4 lines be at the end
+o.autoindent = true                 -- automatic indent new lines
+o.smartindent = true                -- be smart about it
+o.wrap = false                      -- do not wrap lines
+o.expandtab = true                  -- expand tabs to spaces
+o.smarttab = false                  -- fuck tabs
+o.formatoptions:append("n")         -- support for numbered/bullet lists
+o.virtualedit = "block"             -- allow virtual edit in visual block ..
+o.scrolloff = 4                     -- scroll down and let 4 lines be at the end
 
 o.isk:append({"_", "$", "@", "%", "#", "-"}) -- word splitter chars
 vim.cmd("filetype plugin indent on")    -- load filetype plugin
