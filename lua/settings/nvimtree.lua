77831cfe1682f40757b4c49d9a1b01a0b40504c9
Florian Baumann <flo@noqqe.de>
2023-03-20 18:00:15 +0100

    add nvimtree

---
+++ lua/settings/nvimtree.lua
@@ -0,0 +1,31 @@
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+-- this autocloses nvimtree if still open
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
+
+
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup#opening-nvim-tree-at-neovim-startup
+-- this automatically starts it
+local function open_nvim_tree(data)
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  -- only files please
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
